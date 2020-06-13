68f4c6ed37a5f55f10027a19c6864fd9c184681c
Brian Savignano <ataxica@gmail.com>
2020-06-12 17:01:59 -0700

    one more cors fix

--- server.js
+++ server.js
@@ -8,22 +8,7 @@ const config = require('./config');
 
 const streams = require('./routes/api/streams');
 
-var allowedOrigins = ['http://localhost:3000',
-                      'http://protestwatch.live',
-                      'http://www.protestwatch.live'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors({ origin: "*" }));
 
 // BodyParser Middleware
 app.use(bodyParser.json());
