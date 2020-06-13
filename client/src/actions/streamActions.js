8c0fc396181a1ffbedb7d83e6cce663a93f3bb91
Brian Savignano <ataxica@gmail.com>
2020-06-12 17:00:19 -0700

    fixing cors issue

--- client/src/actions/streamActions.js
+++ client/src/actions/streamActions.js
@@ -1,12 +1,10 @@
 import { GET_STREAMS, ADD_STREAM, DELETE_STREAM, STREAMS_LOADING } from './types';
 import axios from 'axios';
-const config = require('./config');
-
-this.axios.defaults.baseURL = config.BASE_URL || "http://localhost:5000";
+const config = require('../config');
 
 export const getStreams = () => dispatch => {
     dispatch(setStreamsLoading());
-    axios.get('/api/streams')
+    axios.get(`${config.BASE_URL}/api/streams`)
         .then(res => dispatch({
             type: GET_STREAMS,
             payload: res.data
@@ -14,7 +12,7 @@ export const getStreams = () => dispatch => {
 }
 
 export const addStream = (stream) => dispatch => {
-    axios.post('/api/streams', stream)
+    axios.post(`${config.BASE_URL}/api/streams`, stream)
         .then(res => dispatch({
             type: ADD_STREAM,
             payload: res.data
@@ -22,7 +20,7 @@ export const addStream = (stream) => dispatch => {
 }
 
 export const deleteStream = (id) => dispatch => {
-    axios.delete(`/api/streams/${id}`)
+    axios.delete(`${config.BASE_URL}/api/streams/${id}`)
         .then(dispatch({
             type: DELETE_STREAM,
             payload: id
